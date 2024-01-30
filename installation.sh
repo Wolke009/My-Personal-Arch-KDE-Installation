5dc48f03b3d2b907687e6514638d42b342376d61
Wolke <98122676+Wolke009@users.noreply.github.com>
2024-01-30 19:51:40 +0000

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -1,37 +1,37 @@
 #!/bin/sh
 #KDE Plasma Installation
 echo "Updating the packages"
-sudo pacman -Syu
+sudo pacman --noconfirm -Syu
 echo "Installing plasma"
-sudo pacman -S plasma-meta
-pacman -S --needed xorg sddm
+sudo pacman --noconfirm -S plasma-meta
+pacman -S --noconfirm --needed xorg sddm
 sudo systemctl enable sddm
 sudo systemctl enable NetworkManager
 
 #Other Programs
 echo "Installing Dolphin(Filesystemmanager)"
-sudo pacman -S dolphin
+sudo pacman --noconfirm -S dolphin
 echo "Installing kitty(Terminalemulator)"
-sudo pacman -S kitty
+sudo pacman --noconfirm -S kitty
 
 #Yay-Installaion
 echo "Installing yay"
-sudo pacman -S --needed base-devel git
+sudo pacman -S --noconfirm --needed base-devel git
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
 #Install Discord(Electron)
 echo "Installing Discord"
-yay -Syu discord_arch_electron
+yay --noconfirm -Syu discord_arch_electron
 
 #Install Librewolf
 echo "Installing Librewolf"
-yay -Syu librewolf-bin
+yay --noconfirm -Syu librewolf-bin
 
 #Installing VSCodium
 echo "Installing VSCodium"
-yay -S vscodium-bin
+yay --noconfirm -S vscodium-bin
 
 #Ending
 echo "Completed"
